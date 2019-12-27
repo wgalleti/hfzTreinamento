21f1a3d92a5ce98f4bba55a98426b710363420e2
William Galleti <william.galleti@gmail.com>
2019-12-27 11:10:54 -0500

    Adicionado navigation drawer

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,27 +1,48 @@
 <template>
-  <v-app id="inspire">
+  <v-app id="app">
+    <v-navigation-drawer
+      v-model="drawer"
+      :clipped="$vuetify.breakpoint.lgAndUp"
+      app
+    >
+      <v-list
+        dense
+        flat
+      >
+        <v-list-item
+          v-for="item in $router.options.routes"
+          :key="item.text"
+          :to="item.path"
+          link
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.name }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar
       :clipped-left="$vuetify.breakpoint.lgAndUp"
       app
       color="blue darken-3"
       dark
     >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title
         class="ml-0 pl-4"
       >
         <span class="hidden-sm-and-down">Hifuzion Financeiro</span>
       </v-toolbar-title>
       <v-spacer />
-
-      <v-btn
-        text
-        :to="rota.path"
-        v-for="rota in $router.options.routes"
-        :key="rota.path"
-      >
-        {{rota.name}}
+      <v-btn icon>
+        <v-icon>mdi-help-circle</v-icon>
       </v-btn>
-
     </v-app-bar>
 
     <v-content>
@@ -31,3 +52,13 @@
     </v-content>
   </v-app>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: null
+    }
+  }
+}
+</script>
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -7,26 +7,31 @@ const routes = [
   {
     path: '/',
     name: 'Home',
+    icon: 'mdi-home',
     component: () => import('@/views/Home.vue')
   },
   {
     path: '/fornecedores',
     name: 'Fornecedores',
+    icon: 'mdi-message',
     component: () => import('@/views/Fornecedores.vue')
   },
   {
     path: '/contas',
     name: 'Contas',
+    icon: 'mdi-history',
     component: () => import('@/views/Contas.vue')
   },
   {
     path: '/titulos',
     name: 'Títulos',
+    icon: 'mdi-settings',
     component: () => import('@/views/Titulos.vue')
   },
   {
     path: '/pagamentos',
     name: 'Pagamentos',
+    icon: 'mdi-keyboard',
     component: () => import('@/views/Pagamentos.vue')
   }
 ]
